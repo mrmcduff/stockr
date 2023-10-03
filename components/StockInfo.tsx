85458587dc0ec74df21b3253b529660c1e85ace8
Michael McDuffee <mrmcduff@gmail.com>
2023-10-03 16:41:23 -0400

    handling cors, making a call to the api

--- components/StockInfo.tsx
+++ components/StockInfo.tsx
@@ -5,15 +5,17 @@ import ReactMarkdown from "react-markdown";
 export type StockInfoProps = {
   symbol: string;
   link: string;
+  name: string;
   info: string;
 };
 
-const Post: React.FC<StockInfoProps> = ({ symbol, link, info }) => {
+const Post: React.FC<StockInfoProps> = ({ symbol, link, info, name }) => {
   return (
     <div>
       <h2>{symbol}</h2>
-      <small>${link}</small>
-      <p>${info}</p>
+      <small>{link}</small>
+      <h4>{name}</h4>
+      <p>{info}</p>
       <style jsx>{`
         div {
           color: inherit;
